5976deb21fbfeb8eccc712133cb2cd8bb74843c4
Aina <ainaard@mail.ru>
2020-07-13 19:41:08 +0300

    initial commit

---
+++ src/test/java/ru/itis/java_lab/JavaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.java_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
