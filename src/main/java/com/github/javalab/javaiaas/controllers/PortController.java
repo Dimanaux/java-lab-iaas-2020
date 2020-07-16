3da7158ac113f1c18ff6bb57f76d1e18616fe98a
amourety <amourety@gmail.com>
2020-07-16 22:06:50 +0300

    Added EnvironmentVariables

---
+++ src/main/java/com/github/javalab/javaiaas/controllers/PortController.java
@@ -0,0 +1,29 @@
+package com.github.javalab.javaiaas.controllers;
+
+
+import com.github.javalab.javaiaas.services.PortService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PortController {
+
+    @Autowired
+    PortService portService;
+
+    @GetMapping("/ports")
+    public ResponseEntity<?> get(){
+        return ResponseEntity.ok(portService.getFreePorts());
+    }
+
+    @GetMapping("/port/{number}")
+    public ResponseEntity<?> check(@PathVariable("number") Long numberPort){
+        return ResponseEntity.ok(!portService.pingHost(numberPort.intValue()));
+    }
+
+}
---
+++ src/main/java/com/github/javalab/javaiaas/services/PortService.java
@@ -0,0 +1,61 @@
+package com.github.javalab.javaiaas.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class PortService {
+
+    public static HashMap<Integer, Boolean> ports = new HashMap<>();
+
+    @Value("${port.service.host.name}")
+    private String host;
+
+    @Value("${port.service.start}")
+    private int lastIndex;
+
+    @Value("${port.service.timeout}")
+    private int timeout;
+
+    public boolean pingHost(int port) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            socket.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public ArrayList<String> getFreePorts(){
+
+        ArrayList<String> freePorts = new ArrayList<>();
+
+        for(int index = 0; index < 20; lastIndex++) {
+            if (lastIndex > 9999) {
+                lastIndex = 1;
+            }
+            if (ports.get(lastIndex)){
+                ports.put(lastIndex, false);
+                freePorts.add(Integer.toString(lastIndex));
+                index++;
+            }
+        }
+        return freePorts;
+    }
+
+    @Scheduled(fixedRate = 1000000)
+    public void checkPort(){
+        for (int i = 1; i <= 9999; i++){
+            boolean isFree = pingHost(i);
+            ports.put(i, isFree);
+        }
+    }
+}
