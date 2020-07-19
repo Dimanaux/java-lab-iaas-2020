cea3f11ec03bdc38c41d337a4baac05c1f594873
Олег Синицин <olejiksin@mail.ru>
2020-07-19 21:34:42 +0300

    add copies of instances

---
+++ src/main/java/com/github/javalab/javaiaas/models/Instance.java
@@ -0,0 +1,36 @@
+package com.github.javalab.javaiaas.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "instances")
+public class Instance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long instanceId;
+    private String instanceUrl;
+    private String instanceName;
+    private Long port;
+    private String repoName;
+    private String status;
+
+
+    @ManyToOne
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JsonIgnore
+    @JoinColumn(name = "user_id")
+    private User user;
+}
